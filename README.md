907c09d669782bdd6146ca59aefcc8b990554df5
jlaustill <jlaustill@gmail.com>
2023-11-23 14:55:04 -0700

    100% coverage, yay

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # bit-utils
 A micro library of bit helpers to make c++ more readable
+
+## Provides the following functions
+# getNBits
+unsigned getNBits(unsigned value, unsigned offset, unsigned n)
+
+This method takes in a value, and returns the value by bits from the offset for n bits. 
+
+A simple example would be 
+0b11 = getNBits(0b100**11**000, 4, 2)
+
+Important things to note are that offset is 0 based, and bits are read from right to left in natural order
--- tests/bit_utils_test.cpp
+++ tests/bit_utils_test.cpp
@@ -2,8 +2,20 @@
 #include "../bin-utils.h"
 
 // Demonstrate some basic assertions.
-TEST(BitUtilsTest, BasicAssertions) {
+TEST(BitUtilsTest, BasicBehaviorWorks) {
     uint8_t result = getNBits(0b11100000, 5, 3);
     // Expect equality.
     EXPECT_EQ(result, 0b111);
+
+    result = getNBits(0b10101101, 2, 4);
+    EXPECT_EQ(result, 0b1011);
+}
+
+TEST(BitUtilsTest, HandlesOutOfBounds) {
+    uint8_t result = getNBits(0b1111, 999, 1);
+    EXPECT_EQ(result, 0);
+
+    result = getNBits(0b1111, 0, 50000000);
+    EXPECT_EQ(result, 0b1111);
+
 }
