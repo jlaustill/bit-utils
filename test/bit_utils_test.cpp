29587a00666f2d909d41f02b4df802ac3b770535
jlaustill <jlaustill@gmail.com>
2023-11-24 11:14:14 -0700

    v1.0.3 with set bits functions

--- test/bit_utils_test.cpp
+++ test/bit_utils_test.cpp
@@ -3,7 +3,7 @@
 #include "bit-utils.h"
 
 // Demonstrate some basic assertions.
-TEST(BitUtilsTest, BasicBehaviorWorks) {
+TEST(BitUtilsTest, getNBitsWorks) {
   uint8_t result = getNBits(0b11100000, 5, 3);
   // Expect equality.
   EXPECT_EQ(result, 0b111);
@@ -12,10 +12,40 @@ TEST(BitUtilsTest, BasicBehaviorWorks) {
   EXPECT_EQ(result, 0b1011);
 }
 
-TEST(BitUtilsTest, HandlesOutOfBounds) {
+TEST(BitUtilsTest, getNBitsHandlesOutOfBounds) {
   uint8_t result = getNBits(0b1111, 999, 1);
   EXPECT_EQ(result, 0);
 
   result = getNBits(0b1111, 0, 50000000);
   EXPECT_EQ(result, 0b1111);
+}
+
+TEST(BitUtilsTest, getNOnes) {
+  uint32_t result = getNOnes(4);
+
+  EXPECT_EQ(result, 0b1111);
+}
+
+TEST(BitUtilsTest, getNOnesAt) {
+  uint32_t result = getNOnesAt(4, 4);
+
+  EXPECT_EQ(result, 0b11110000);
+}
+
+TEST(BitUtilsTest, setNBits) {
+  uint32_t result = setNBitsAt(0b0001, 0b11, 2, 2);
+
+  EXPECT_EQ(result, 0b1101);
+}
+
+TEST(BitUtilsTest, setNBitsAtOutOfBounds) {
+  uint32_t result = setNBitsAt(0b01, 0b11111111, 999, 2);
+
+  EXPECT_EQ(result, 0b01);
+}
+
+TEST(BitUtilsTest, setNBitsAtConstrainBits) {
+  uint32_t result = setNBitsAt(0b01, 0b111111, 1, 1);
+
+  EXPECT_EQ(result, 0b11);
 }
