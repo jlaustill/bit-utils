84709cc053d0334657eafc51efbd54c50837dba0
jlaustill <jlaustill@gmail.com>
2023-11-23 18:41:52 -0700

    switch to platformio

---
+++ test/bit_utils_test.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+#include "bit-utils.h"
+
+// Demonstrate some basic assertions.
+TEST(BitUtilsTest, BasicBehaviorWorks) {
+  uint8_t result = getNBits(0b11100000, 5, 3);
+  // Expect equality.
+  EXPECT_EQ(result, 0b111);
+
+  result = getNBits(0b10101101, 2, 4);
+  EXPECT_EQ(result, 0b1011);
+}
+
+TEST(BitUtilsTest, HandlesOutOfBounds) {
+  uint8_t result = getNBits(0b1111, 999, 1);
+  EXPECT_EQ(result, 0);
+
+  result = getNBits(0b1111, 0, 50000000);
+  EXPECT_EQ(result, 0b1111);
+}
